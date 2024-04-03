d99d2b43949689f57a23eef3f331a60e50fe391b
zalvladik <zalipkav@ukr.net>
2024-04-03 20:20:19 +0300

    removed build

--- backend/build/e2e/board/responses.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const responses = {
-    get: (data, boardId) => {
-        return data.find(item => item.id === boardId);
-    },
-};
-exports.default = responses;
-//# sourceMappingURL=responses.js.map
--- backend/build/e2e/section/responses.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const responses = {
-    get: (data, sectionId) => {
-        return data.find(item => item.id === sectionId);
-    },
-};
-exports.default = responses;
-//# sourceMappingURL=responses.js.map
